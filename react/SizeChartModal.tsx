a7bfef7fd59354dcd5dcb5b51ba3c07acb7fe6cb
Web Developer <useraccount@WebDeveloperMBP.local>
2023-01-30 13:47:21 -0600

    Reformat Category Data - LM

--- react/SizeChartModal.tsx
+++ react/SizeChartModal.tsx
@@ -11,91 +11,53 @@ interface LinkObject {
   link: string
 }
 
-// Where to place trigger button
-const targetTriggerContainerClass = "vtex-store-components-3-x-skuSelectorSubcontainer--size";
-
 // Size Chart Class currently used in all Size Chart fields in VTEX's Product Admin.
 const sizeChartClass = "vtex-size-chart-content";
+
+// Class of second tier breadcrumb for Size Chart Category. Inner Text contains "Bicycles", "Snowboards", ect.
+const categoryClass = "vtex-breadcrumb-1-x-arrow--2";
 
 // List of all pages of "general sizing information" for products.
 const sizeChartList = [{ category: "Bicycles", link: "/bike-sizing" }, { category: "Snowboards", link: "/sizing-a-snowboard" }, { category: "Skis", link: "/ski-sizing" }, { category: "Shoes", link: "/cycling-shoes-buying-guide#section-4" }];
 
 const SizeChartModal: StorefrontFunctionComponent<SizeChartModalProps> = ({ }) => {
-  const timerGateOpen = useRef(true);
-  const productCategories = useRef<any>();
+  const gateOpen = useRef(true);
   const body = useRef<any>();
-  const targetTriggerContainer = useRef<any>();
-  const device = useRef("");
   const [renderModalControls, setRenderModalControls] = useState(false);
   const [renderLink, setRenderLink] = useState(false);
   const [linkData, setLinkData] = useState<LinkObject>();
   const [modalActive, setModalActive] = useState(false);
   const [modalData, setModalData] = useState("");
 
-  const grabDOM = (selector: string) => canUseDOM ? document.querySelector(selector) : null;
+  const grabDOM: any = (selector: string) => canUseDOM ? document.querySelector(selector) : null;
 
-  // Adds and removes Event Listeners.
+  // Component Did Mount.
   useEffect(() => {
-    if (!canUseDOM) return;
-    window.addEventListener("message", handleMessage);
-    window.addEventListener("keydown", escapeModal);
-    window.addEventListener("resize", resizeWindow);
+    if (!gateOpen.current) return;
+    gateOpen.current = false;
 
-    return () => {
-      window.removeEventListener("message", handleMessage);
-      window.removeEventListener("keydown", escapeModal);
-      window.removeEventListener("resize", resizeWindow);
-    }
+    resetPage();
+    findChart();
   })
 
-  // Throttle operations to once per 2 seconds.
-  const resizeWindow = () => {
-    if (!timerGateOpen.current) return;
-    timerGateOpen.current = false;
+  // Adds and removes Event Listener.
+  useEffect(() => {
+    if (!canUseDOM) return;
+    window.addEventListener("keydown", escapeModal);
 
-    setTimeout(() => {
-      if (!canUseDOM) return;
-      const innerWidth = window.innerWidth;
-
-      const desktopToMobile = device.current === "desktop" && innerWidth <= 1025;
-      const mobileToDesktop = device.current === "mobile" && innerWidth >= 1026;
-
-      if (desktopToMobile || mobileToDesktop) resetPage();
-
-      timerGateOpen.current = true;
-    }, 2000)
-  }
-
-  // Listens for messages from VTEX.
-  const handleMessage = (e: any) => {
-    const eventName = e.data.eventName;
-
-    if (eventName === "vtex:pageView") {
-      setRenderModalControls(false);
-      setModalActive(false);
+    return () => {
+      window.removeEventListener("keydown", escapeModal);
     }
-
-    if (eventName === "vtex:productView") {
-      productCategories.current = e.data.product.categoryTree;
-      resetPage();
-    }
-  }
+  })
 
   // Reset all state and refs for new data.
   const resetPage = () => {
     if (!canUseDOM) return;
     body.current = document.body;
-    device.current = window.innerWidth >= 1026 ? "desktop" : "mobile";
 
     setRenderModalControls(false);
     setModalActive(false);
     setRenderLink(false);
-
-    // Timeout for slowing down state management and for letting VTEX update their DOM.
-    setTimeout(() => {
-      targetTriggerContainer.current = grabDOM(`.${targetTriggerContainerClass}`);
-      findChart();
-    }, 250);
   }
 
   // Size Chart container <div> is loaded in below the breadcrumbs and hidden with CSS.
@@ -119,23 +81,18 @@ const SizeChartModal: StorefrontFunctionComponent<SizeChartModalProps> = ({ }) =
     setRenderModalControls(true);
   }
 
-  // Loops through sizeChartList array and compares productCategories.current
+  // Loops through sizeChartList array and compares productCategory
   // to find a match. If a match is found, we render a link.
   const findLink = () => {
-    let breakParentLoop = false;
+    const categoryDOM = grabDOM(`.${categoryClass}`);
+    const productCategory = categoryDOM?.innerText;
 
-    for (let sizeChartIndex = 0; sizeChartIndex < sizeChartList.length; sizeChartIndex++) {
-      if (breakParentLoop) break;
-      const category = sizeChartList[sizeChartIndex].category;
+    if (!productCategory) return;
 
-      for (let productCatIndex = 0; productCatIndex < productCategories.current.length; productCatIndex++) {
-        const categoryName = productCategories.current[productCatIndex].name;
-        // If a general page exists for the current product category, show link and break loops.
-        if (categoryName === category) {
-          showLink(sizeChartIndex);
-          breakParentLoop = true;
-          break;
-        }
+    for (let categoryIndex = 0; categoryIndex < sizeChartList.length; categoryIndex++) {
+      if (productCategory === sizeChartList[categoryIndex].category) {
+        showLink(categoryIndex);
+        break;
       }
     }
   }
@@ -185,11 +142,7 @@ const SizeChartModal: StorefrontFunctionComponent<SizeChartModalProps> = ({ }) =
     </div>
   )
 
-  // Portals are necessary due to smaller screens loading the desktop version first, then
-  // re-rendering the mobile version. This wipes out and reloads the app AFTER the "vtex:productView"
-  // has fired so we lose the information. To fix this we place this app outside of the context of the 
-  // re-rendering blocks and attach a "page resize" listener to call the app to look for assets again. - LM
-  return renderModalControls ? !modalActive ? createPortal(<ModalTrigger />, targetTriggerContainer.current) : createPortal(<PopUp />, body.current) : renderLink ? createPortal(<LinkComponent />, targetTriggerContainer.current) : <></>;
+  return renderModalControls ? !modalActive ? <ModalTrigger /> : createPortal(<PopUp />, body.current) : renderLink ? <LinkComponent /> : <></>;
 }
 
 SizeChartModal.schema = {
